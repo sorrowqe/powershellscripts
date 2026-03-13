41c0686405a47fbf9978ea6358f65dadeeec4567
aorta <ID+USERNAME@users.noreply.github.com>
2026-03-13 09:19:52 +0200

    Ülesanne 1: Kasutaja loomine ees- ja perenime sisestaimsega skriptis

--- Ülesanne1.ps1
+++ Ülesanne1.ps1
@@ -1,13 +1,30 @@
-$ees = Read-Host "Sisestage enda eesnimi: "
-$pere = Read-Host "Sisestage enda perenimi: "
+# Lokaalse kasutaja loomise skript
 
+$ees = Read-Host "Sisestage enda eesnimi"
+$pere = Read-Host "Sisestage enda perenimi"
+
+# Kasutajanimi kujul ees.perenimi (väiketähtedega)
 $nimi = ("$ees.$pere").ToLower()
 $taisnimi = "$ees $pere"
+$kirjeldus = "Kasutaja $taisnimi"
 
 $parool = ConvertTo-SecureString "Parool1!" -AsPlainText -Force
 
-if ($?) {
-    Write-Host "Kasutaja loodud: $nimi"
-} else {
-    Write-Host "Kasutaja loomine ebaõnnestus!"
+# Proovime lokaalse kasutaja luua
+try {
+    New-LocalUser -Name $nimi `
+                  -FullName $taisnimi `
+                  -Description $kirjeldus `
+                  -Password $parool `
+                  -ErrorAction Stop
+
+    Write-Host "Kasutaja loodud: $nimi" -ForegroundColor Green
+}
+catch {
+    # Kontrollime, kas probleem on olemasoleva kasutajaga
+    if ($_.Exception.Message -like "*already exists*" -or $_.CategoryInfo.Reason -eq "UserExistsException") {
+        Write-Host "Kasutaja loomine ebaõnnestus! Kasutaja '$nimi' on juba olemas." -ForegroundColor Red
+    } else {
+        Write-Host "Kasutaja loomine ebaõnnestus! Viga: $($_.Exception.Message)" -ForegroundColor Red
+    }
 }
